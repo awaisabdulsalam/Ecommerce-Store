452815173810a21eb8e80e6bfb4f6a0df379a1e0
Awais Abdul Salam <theawais170@gmail.com>
2023-05-16 02:43:13 +0500

    deploed

--- my-app/src/main.jsx
+++ my-app/src/main.jsx
@@ -1,13 +1,12 @@
 // import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import 'tailwindcss/tailwind.css';
-import { Provider } from 'react-redux';
-import store from "./store/index.js"
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { Provider } from "react-redux";
+import store from "./store/index.js";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <Provider store={store}>
-    <App />  
+    <App />
   </Provider>
-)
+);
