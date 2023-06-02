e4d50914c9606477fdb41abf22b319535dfc961a
Awais Abdul Salam <theawais170@gmail.com>
2023-06-03 01:32:23 +0500

    notification

--- my-app/src/components/Header.jsx
+++ my-app/src/components/Header.jsx
@@ -1,3 +1,4 @@
+import { FaShoppingCart  } from 'react-icons/fa';
 import { Link } from "react-router-dom";
 import { useSelector } from "react-redux";
 
@@ -10,15 +11,14 @@ const Header = () => {
     <>
     <nav>
       <div>
-        <a href="/Ecommerce-Store"></a>
-        <img src="./images/eco-logo.png" alt="Log" />
-       <h2>Eco Logo</h2>
+       <h2 className="logo">Eco Logo</h2>
       </div>
       <div>
         <ul>
           <li className="nav_list"><Link to="/Ecommerce-Store">Home</Link></li>
           <li className="nav_list"><Link to="/Ecommerce-Store/products">Products</Link></li>
-          <li className="nav_list"><Link to="/Ecommerce-Store/card">Cart{`(${cart})`}</Link></li>
+          <li className="nav_list"><Link to="/Ecommerce-Store/card">
+            <FaShoppingCart  className='shopping-cart' /><span className={cart.length === 0 ? 'notification_hide' : 'notification_show'}>{`${cart}`}</span></Link></li>
         </ul>
       </div>
     </nav>
