96f435294517c247a19514c84aa2424cd4e064d6
Awais Abdul Salam <theawais170@gmail.com>
2023-06-03 03:14:51 +0500

    nav

--- my-app/src/components/Header.jsx
+++ my-app/src/components/Header.jsx
@@ -1,12 +1,15 @@
-import { FaShoppingCart  } from 'react-icons/fa';
+import { FaShoppingCart, FaSearch  } from 'react-icons/fa';
 import { Link, useLocation } from "react-router-dom";
 import { useSelector } from "react-redux";
+import { useState } from 'react';
 
 const Header = () => {
   const data = useSelector((state) => {
     return state.users
   });
-  
+
+  const [inputText, setInputText] = useState('');
+
   const cart = data.length;
 
   const location = useLocation();
@@ -24,7 +27,15 @@ const Header = () => {
         <ul>
           <li className={`nav_list ${isActivated("/Ecommerce-Store") ? "active" : ""}`}><Link to="/Ecommerce-Store">Home</Link></li>
           <li className={`nav_list ${isActivated("/Ecommerce-Store/products") ? "active" : ""}`}><Link to="/Ecommerce-Store/products">Products</Link></li>
-          <input type="text" placeholder='Search' />
+          <li className='nav_list search_icon_wrapper'>
+          <input type="text"
+          placeholder='Search'
+          className='search_input'
+          value={inputText}
+          onChange={(e) => setInputText(e.target.value)}
+          />
+          <FaSearch  className='shopping_cart' />
+          </li>
           <li className={`nav_list ${isActivated("/Ecommerce-Store/card") ? "active" : ""}`}><Link to="/Ecommerce-Store/card">
             <FaShoppingCart  className='shopping-cart' /><span className={cart === 0 ? 'notification_hide' : 'notification_show'}>{`${cart}`}</span></Link></li>
         </ul>
