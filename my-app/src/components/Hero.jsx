b1ef85279c7eb4e43769ff435a7ac74135e4bce2
Awais Abdul Salam <theawais170@gmail.com>
2023-06-05 23:23:33 +0500

    fixed slider

--- my-app/src/components/Hero.jsx
+++ my-app/src/components/Hero.jsx
@@ -3,17 +3,23 @@ import { useState } from "react";
 import image1 from "../images/double-sofa-01.png";
 import image2 from "../images/double-sofa-02.png";
 import image3 from "../images/double-sofa-03.png";
+import { current } from "@reduxjs/toolkit";
 
 const Hero = () => {
   const imageArray = [image1, image2, image3];
-  const images = imageArray.map((image) => {
-    // console.log(image);
-    return image;
-  });
-  const [count, setCount] = useState(0);
-  const [sliderImage, setSliderImage] = useState(images[count]);
-  console.log(count);
-  console.log(sliderImage);
+  // const images = imageArray.map((image) => {
+  //   // console.log(image);
+  //   return image;
+  // });
+  const [sliderImage, setSliderImage] = useState(0);
+
+  const prevImage = () => {
+    setSliderImage(sliderImage === 0 ? imageArray.length - 1 : sliderImage - 1);
+  };
+  const nextImage = () => {
+    setSliderImage(sliderImage === imageArray.length - 1 ? 0 : sliderImage + 1);
+  };
+
   return (
     <main className="hero_main">
       <section className="hero_section">
@@ -35,16 +41,18 @@ const Hero = () => {
                 <button className="hero_btn">Visit Now</button>
               </div>
             </div>
-            <div>
-              <img
-                src={`${sliderImage}`}
-                alt="Image"
-                className="slider_image"
-              />
-            </div>
+            {imageArray.map((image, index) => {
+              return (
+                sliderImage === index && (
+                  <div key={index}>
+                    <img src={image} alt="Image" className="slider_image" />
+                  </div>
+                )
+              );
+            })}
           </div>
-          <FaArrowLeft onClick={() => setCount(count - 1)} />
-          <FaArrowRight onClick={() => setCount()} />
+          <FaArrowLeft onClick={prevImage} />
+          <FaArrowRight onClick={nextImage} />
         </section>
       </section>
     </main>
